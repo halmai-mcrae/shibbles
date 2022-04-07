fd8d33b557862ff0831175954c9a81e23cbab071
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-04-08 08:46:16 +1200

    Set up repo

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="title">
+        <h1>SHIBBLES</h1>
+      </div>
+      {/* This 'main' div is only for styling (so we can use flexbox) */}
+      <div className="main">
+      </div>
+    </>
+  )
+}
+
+export default App
